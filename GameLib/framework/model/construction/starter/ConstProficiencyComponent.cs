68ac64cb75862a82d68579dc180276036703598a
hundun <wwj.hddjj@gmail.com>
2023-04-22 19:35:22 +0800

    add starter

---
+++ GameLib/framework/model/construction/starter/ConstProficiencyComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hundun.idleshare.gamelib
+{
+
+    public class ConstProficiencyComponent : ProficiencyComponent
+    {
+
+        public ConstProficiencyComponent(BaseConstruction construction) : base(construction)
+        {
+
+        }
+
+        public override void onSubLogicFrame()
+        {
+            // do nothing
+        }
+
+        public override void afterUpgrade()
+        {
+            // do nothing
+        }
+    }
+}
